ba155a4c5a528686d6fd7ff972db97449309b2e8
Temu-15 <abuadanu89@gmail.com>
2023-11-12 16:55:11 +0300

    add contact form

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -19,21 +19,18 @@ var typed = new Typed(".career", {
   loop: true,
 });
 
-document.querySelector("btn-contacts").addEventListener("click", (e) => {
-  // stop the page from the reloading
-  e.preventDefault();
-  // selecting the form
-  const contactForm = document.querySelector("#form");
-  // the gold part 🔥
-  let data = new FormData(contactForm);
-  fetch("http://127.0.0.1:5500/Components/Contacts.html", {
-    method: "POST",
-    body: data,
-  });
-  // inform the user that the form was submitted
-  alert("Thank you, your form was submitted.");
-  // clear the inputs
-  contactForm.reset();
-  // or if you want the redirect the user to another page (for free)
-  // window.open('./thanks.html');
-});
+// document.querySelector("btn-contacts").addEventListener("click", (e) => {
+//   e.preventDefault();
+
+//   const contactForm = document.querySelector("#form");
+
+//   let data = new FormData(contactForm);
+//   fetch("http://127.0.0.1:5500/Components/Contacts.html", {
+//     method: "POST",
+//     body: data,
+//   });
+
+//   alert("Thank you, your form was submitted.");
+
+//   contactForm.reset();
+// });
